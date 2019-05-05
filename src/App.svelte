c7bd0dcb19ca3a1fb37432ba11884d097f053694
Ahmad <a_050313@Ahmads-MacBook-Pro.local>
2019-05-05 15:03:36 +0700

    Init commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Dashboard from "./screens/Dashboard/DashboardComponent";
+	export let name;
+</script>
+
+<style>
+	h1 {
+	  color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+
+<Dashboard/>
---
+++ src/screens/Dashboard/DashboardComponent.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { onMount } from "svelte";
+  import axios from "axios";
+
+  const appBaseUrl = "https://chatter-api.azurewebsites.net/api/";
+
+  const api = axios.create({
+    baseURL: appBaseUrl,
+    timeout: 15000,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  let chatter = {
+    title: "",
+    description: "",
+    source: ""
+  };
+
+  async function getChatter() {
+    const response = await api.get("/chatter");
+
+    chatter = response.data;
+  }
+
+  onMount(getChatter);
+</script>
+
+
+<div>This is Dashboard</div>
+<div>{chatter.title}</div>
+<div>{chatter.description}</div>
+<div>{chatter.source}</div>
